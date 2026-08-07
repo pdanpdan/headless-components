18ae4ca3d3b5bfb9f34dff563f9f243791e04746
Dan Popescu <pdan.popescu@gmail.com>
2026-08-07 13:17:06 +0300

    feat(playground): add MDI icon component using @mdi/js

---
+++ packages/playground/components/MdiIcon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  path: string;
+}>();
+</script>
+
+<template>
+  <svg
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    aria-hidden="true"
+  >
+    <path :d="path" />
+  </svg>
+</template>
